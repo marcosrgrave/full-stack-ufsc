3d01dea1df954588367323374f6a601061c1dd32
bawa11 <marcos.r.grave@gmail.com>
2022-08-07 16:23:01 -0300

    document format

--- firstProject/src/lessons/Lesson1.java
+++ firstProject/src/lessons/Lesson1.java
@@ -1,23 +1,24 @@
 package lessons;
+
 import java.util.Arrays;
 
 public class Lesson1 {
     public static void main(String[] args) throws Exception {
         // this is a single line comment
-        
+
         /*
          * multiple lines comment
          */
 
         System.out.println("Hello, World!");
-        
+
         float number = 10.5f;
         System.out.println(number);
-        
+
         String str = "this is a string";
         System.out.println(str);
 
-        for (int i = 0; i <= 4; i = i+2) {
+        for (int i = 0; i <= 4; i = i + 2) {
             System.out.println(i);
         }
 
