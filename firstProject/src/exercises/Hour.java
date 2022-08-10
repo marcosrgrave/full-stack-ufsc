d5a8c4638844fb71098cf6094e8523bbdd8743ab
bawa11 <marcos.r.grave@gmail.com>
2022-08-09 23:10:01 -0300

    Created the basic functionality to Hour class

---
+++ firstProject/src/exercises/Hour.java
@@ -0,0 +1,39 @@
+package exercises;
+
+public class Hour {
+
+    private int hour;
+    private int minute;
+    private int second;
+
+    public Hour(int hour, int minute, int second) {
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+
+    public int getHour() {
+        return this.hour;
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    public int getMinute() {
+        return this.minute;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+
+    public int getSecond() {
+        return this.second;
+    }
+
+    public void setSecond(int second) {
+        this.second = second;
+    }
+
+}
